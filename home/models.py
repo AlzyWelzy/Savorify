ff0d8ba1d20ef3dee248f34b116b525b478a09fb
AlzyWelzy <welzyalzy@gmail.com>
2023-11-30 23:30:37 +0530

    added files

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Home(models.Model):
+    recipe_name = models.CharField(max_length=100)
+    recipe_description = models.TextField()
+    recipe_image = models.ImageField(upload_to="images/", null=True)
--- home/views.py
+++ home/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Home
+import datetime
 
 # Create your views here.
+
+
+def index(request):
+    context = {
+        "title": "Savorify",
+        "year": datetime.datetime.now().year,
+    }
+    if request.method == "POST":
+        home = Home()
+        home.recipe_name = request.POST.get("recipe_name")
+        home.recipe_description = request.POST.get("recipe_description")
+        home.recipe_image = request.FILES.get("recipe_image")
+        home.save()
+
+        return redirect("/")
+
+    return render(request, "index.html", context)
