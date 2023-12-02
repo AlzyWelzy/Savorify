805284bdce133bda856510f047ac137f40ecee6b
AlzyWelzy <welzyalzy@gmail.com>
2023-12-02 18:34:36 +0530

    completeing

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("delete_recipe/<int:id>/", views.delete_recipe, name="delete_recipe"),
+    path("update_recipe/<int:id>/", views.update_recipe, name="update_recipe"),
 ]
--- home/views.py
+++ home/views.py
@@ -25,6 +25,27 @@ def index(request):
     return render(request, "index.html", context)
 
 
+def update_recipe(request, id):
+    recipe = Home.objects.get(id=id)
+    context = {
+        "title": "Savorify",
+        "year": datetime.datetime.now().year,
+        "recipe": recipe,
+    }
+
+    if request.method == "POST":
+        recipe.recipe_name = request.POST.get("recipe_name")
+        recipe.recipe_description = request.POST.get("recipe_description")
+
+        if request.FILES.get("recipe_image"):
+            recipe.recipe_image = request.FILES.get("recipe_image")
+
+        recipe.save()
+        return redirect("/")
+
+    return render(request, "update_recipe.html", context)
+
+
 def delete_recipe(request, id):
     recipe = Home.objects.get(id=id)
     recipe.delete()
