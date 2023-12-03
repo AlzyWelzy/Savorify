2b1b0d238aa3c5193a772b0c6f4639e2935eeab9
AlzyWelzy <welzyalzy@gmail.com>
2023-12-03 18:14:52 +0530

    .

--- home/models.py
+++ home/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Home(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     recipe_name = models.CharField(max_length=100)
     recipe_description = models.TextField()
     recipe_image = models.ImageField(upload_to="images/", null=True)
